7b9e17332385ccf222364cde5fc64716c80e0193
Yoran <Yoran.engelberts@gmail.com>
2018-02-21 12:51:54 +0100

    config system

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -1,6 +1,12 @@
 <?php
 
 require_once("includes/config.php");
+require_once("includes/config.template.php");
+
+if(CONFIG_VERSION !== $templateversion){
+    die('invalid config file');
+}
+
 require_once("includes/doctrine.php");
 require_once("includes/oauth2.php");
 require_once('controller/Controller.php');
---
+++ includes/config.template.php
@@ -0,0 +1,16 @@
+<?php
+$templateversion = "0.1.a";
+/*
+the config.php file should be in the same folder and be structured like this:
+
+define("CONFIG_VERSION","0.1.a");
+
+//paths
+define("BASE_URL","http://example.local");
+
+//database
+define("DBHOST","127.0.0.1");
+define("DBUSER","db_user_name");
+define("DBPASS","db_user_password");
+define("DBNAME","db_name");
+*/
